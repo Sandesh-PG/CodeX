547716fa568ae5d0e9ac8b2ad0ecb7ff2d9ca41b
Sandesh-pg <sandeshsandy783@gmail.com>
2023-07-16 23:20:58 +0530

    7/23

--- index.js
+++ index.js
@@ -1,49 +1,54 @@
-const trainingSec = $('#training');
-const dismiss = $('.btn-dismiss');
+// Carousel Animation ---------------------------
 
-function hideTrainingSection() {
-    trainingSec.style.display = 'none';
-};
-
-
-var carouselWidth = $('.carousel-inner')[0].scrollWidth;
-var cardWidth = $('.carousel-item').width();
+var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+var cardWidth = $(".carousel-item").width();
 
 var scrollPosition = 0;
 
-
-$('.carousel-control-next').on('click', function () {
-    if (scrollPosition < (carouselWidth - (cardWidth * 4))) {
-        scrollPosition = scrollPosition + cardWidth;
-        $('.carousel-inner').animate({ scrollLeft: scrollPosition }, 600);
-    }
+$(".carousel-control-next").on("click", function () {
+  if (scrollPosition < carouselWidth - cardWidth * 4) {
+    scrollPosition = scrollPosition + cardWidth;
+    $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600);
+  }
 });
 
-$('.carousel-control-prev').on('click', function () {
-    if (scrollPosition > 0) {
-        scrollPosition = scrollPosition - cardWidth;
-        $('.carousel-inner').animate({ scrollLeft: scrollPosition }, 600);
-    }
+$(".carousel-control-prev").on("click", function () {
+  if (scrollPosition > 0) {
+    scrollPosition = scrollPosition - cardWidth;
+    $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600);
+  }
 });
 
-$('.btn-top').on('click', function () {
-    document.documentElement.scrollTop = 0;
+// ---------------------------
+
+const trainingSec = document.querySelector(".codex-training");
+const dismiss = $(".btn-dismiss");
+
+// Training section dismiss
+dismiss.on("click", function () {
+  trainingSec.style.display = "none";
 });
 
-var navLinks = $('.nav-link');
-var active = $('.active');
+// Scroll To Top ---------------------------
+
+$(".btn-top").on("click", function () {
+  document.documentElement.scrollTop = 0;
+});
+
+// ---------------------------
+
+// NavBar Toggle ---------------------------
+var navLinks = $(".nav-link");
+var active = $(".active");
 
 for (let i = 0; i < navLinks.length; i++) {
-    $(document).on('click', '.nav-link', function () {
-        var navLinks = $('.nav-link');
-        var active = $('.active');
+  $(document).on("click", ".nav-link", function () {
+    var navLinks = $(".nav-link");
+    var active = $(".active");
 
-        active.removeClass('active');
-        $(this).addClass('active');
-    });
-};
-  
+    active.removeClass("active");
+    $(this).addClass("active");
+  });
+}
 
-
-
-
+// ---------------------------
